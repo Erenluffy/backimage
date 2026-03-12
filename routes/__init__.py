87412d0471fa2e4b251fb1517eddb7d890554247
Erenluffy <luffyravi2000@gmail.com>
2026-03-13 01:44:11 +0530

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,13 +1,19 @@
-# Routes package initializer
+# routes/__init__.py
+
 from flask import Blueprint
 
 # Create blueprints
-optimize_bp = Blueprint('optimize', __name__, url_prefix='/api/optimize')
-create_bp = Blueprint('create', __name__, url_prefix='/api/create')
-edit_bp = Blueprint('edit', __name__, url_prefix='/api/edit')
-convert_bp = Blueprint('convert', __name__, url_prefix='/api/convert')
-security_bp = Blueprint('security', __name__, url_prefix='/api/security')
-batch_bp = Blueprint('batch', __name__, url_prefix='/api/batch')
+optimize_bp = Blueprint("optimize", __name__, url_prefix="/api/optimize")
+create_bp = Blueprint("create", __name__, url_prefix="/api/create")
+edit_bp = Blueprint("edit", __name__, url_prefix="/api/edit")
+convert_bp = Blueprint("convert", __name__, url_prefix="/api/convert")
+security_bp = Blueprint("security", __name__, url_prefix="/api/security")
+batch_bp = Blueprint("batch", __name__, url_prefix="/api/batch")
 
-# Import routes to register them
-from . import optimize, create, edit, convert, security, batch
+# Import route modules (this registers their endpoints)
+from . import optimize
+from . import create
+from . import edit
+from . import convert
+from . import security
+from . import batch
